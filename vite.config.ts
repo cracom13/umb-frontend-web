aac5ace530a09200b8b7a175ec5e5871b0f41259
Camilo Gonzalez <andresmartinez.cg@academia.umb.edu.co>
2025-11-17 23:48:33 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
-2️⃣ Copia este contenido dentro
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Reemplaza 'NOMBRE_REPO' por el nombre exacto de tu repositorio en GitHub
 export default defineConfig({
   plugins: [react()],
-  base: "umb-frontend-web",
+  base: "/umb-frontend-web/",  // <--- aquí pones el nombre de tu repo
 });
